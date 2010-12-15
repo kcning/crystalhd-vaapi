986bd913d32bcc41720b5526645848e95275600b
Victor Tseng <palatis@iworldcom.com>
2010-12-15 11:30:26 +0800

    add function declarations to header files

--- src/crystalhd_drv_video.h
+++ src/crystalhd_drv_video.h
@@ -56,7 +56,7 @@
 #define CRYSTALHD_MIN3(a, b, c)	CRYSTALHD_MIN((a), (CRYSTALHD_MIN((b), (c))))
 #define CRYSTALHD_MAX3(a, b, c)	CRYSTALHD_MAX((a), (CRYSTALHD_MAX((b), (c))))
 
-#define INIT_DRIVER_DATA	struct crystalhd_driver_data *driver_data = (struct crystalhd_driver_data *) ctx->pDriverData;
+#define INIT_DRIVER_DATA	struct crystalhd_driver_data *driver_data = get_crystalhd_driver_data(ctx);
 
 #define CONFIG(id)	((object_config_p) object_heap_lookup( &driver_data->config_heap, id ))
 #define CONTEXT(id)	((object_context_p) object_heap_lookup( &driver_data->context_heap, id ))
@@ -100,6 +100,9 @@ struct object_context {
 
 	int last_h264_sps_id;
 	int last_h264_pps_id;
+
+	uint8_t *metadata;
+	int metadata_size;
 };
 
 struct object_surface {
@@ -134,9 +137,370 @@ typedef struct object_buffer *object_buffer_p;
 typedef struct object_image *object_image_p;
 
 static inline struct crystalhd_driver_data *
-crystalhd_driver_data(VADriverContextP ctx)
+get_crystalhd_driver_data(VADriverContextP ctx)
 {
-	return (struct crystalhd_driver_data *)ctx->pDriverData;
+	if (ctx)
+		return (struct crystalhd_driver_data *)ctx->pDriverData;
+	return NULL;
 }
 
+VAStatus crystalhd_QueryConfigProfiles(
+	VADriverContextP ctx,
+	VAProfile *profile_list,	/* out */
+	int *num_profiles		/* out */
+);
+
+VAStatus crystalhd_QueryConfigEntrypoints(
+	VADriverContextP ctx,
+	VAProfile profile,
+	VAEntrypoint  *entrypoint_list,	/* out */
+	int *num_entrypoints		/* out */
+);
+
+VAStatus crystalhd_GetConfigAttributes(
+	VADriverContextP ctx,
+	VAProfile profile,
+	VAEntrypoint entrypoint,
+	VAConfigAttrib *attrib_list,	/* in/out */
+	int num_attribs
+);
+
+VAStatus crystalhd_CreateConfig(
+	VADriverContextP ctx,
+	VAProfile profile,
+	VAEntrypoint entrypoint,
+	VAConfigAttrib *attrib_list,
+	int num_attribs,
+	VAConfigID *config_id		/* out */
+);
+
+VAStatus crystalhd_DestroyConfig(
+	VADriverContextP ctx,
+	VAConfigID config_id
+);
+
+VAStatus crystalhd_QueryConfigAttributes(
+	VADriverContextP ctx,
+	VAConfigID config_id,
+	VAProfile *profile,		/* out */
+	VAEntrypoint *entrypoint, 	/* out */
+	VAConfigAttrib *attrib_list,	/* out */
+	int *num_attribs		/* out */
+);
+
+VAStatus crystalhd_CreateSurfaces(
+	VADriverContextP ctx,
+	int width,
+	int height,
+	int format,
+	int num_surfaces,
+	VASurfaceID *surfaces		/* out */
+);
+
+VAStatus crystalhd_DestroySurfaces(
+	VADriverContextP ctx,
+	VASurfaceID *surface_list,
+	int num_surfaces
+);
+
+VAStatus crystalhd_QueryImageFormats(
+	VADriverContextP ctx,
+	VAImageFormat *format_list,	/* out */
+	int *num_formats		/* out */
+);
+
+VAStatus crystalhd_CreateImage(
+	VADriverContextP ctx,
+	VAImageFormat *format,
+	int width,
+	int height,
+	VAImage *out_image		/* out */
+);
+
+VAStatus crystalhd_DeriveImage(
+	VADriverContextP ctx,
+	VASurfaceID surface,
+	VAImage *image			/* out */
+);
+
+VAStatus crystalhd_DestroyImage(
+	VADriverContextP ctx,
+	VAImageID image
+);
+
+VAStatus crystalhd_SetImagePalette(
+	VADriverContextP ctx,
+	VAImageID image,
+	unsigned char *palette
+);
+
+VAStatus crystalhd_GetImage(
+	VADriverContextP ctx,
+	VASurfaceID surface,
+	int x,			/* coordinates of the upper left source pixel */
+	int y,
+	unsigned int width,	/* width and height of the region */
+	unsigned int height,
+	VAImageID image
+);
+
+VAStatus crystalhd_PutImage(
+	VADriverContextP ctx,
+	VASurfaceID surface,
+	VAImageID image,
+	int src_x,
+	int src_y,
+	unsigned int src_width,
+	unsigned int src_height,
+	int dest_x,
+	int dest_y,
+	unsigned int dest_width,
+	unsigned int dest_height
+);
+
+VAStatus crystalhd_QuerySubpictureFormats(
+	VADriverContextP ctx,
+	VAImageFormat *format_list,	/* out */
+	unsigned int *flags,		/* out */
+	unsigned int *num_formats	/* out */
+);
+
+VAStatus crystalhd_CreateSubpicture(
+	VADriverContextP ctx,
+	VAImageID image,
+	VASubpictureID *subpicture	/* out */
+);
+
+VAStatus crystalhd_DestroySubpicture(
+	VADriverContextP ctx,
+	VASubpictureID subpicture
+);
+
+VAStatus crystalhd_SetSubpictureImage(
+		VADriverContextP ctx,
+		VASubpictureID subpicture,
+		VAImageID image
+);
+
+VAStatus crystalhd_SetSubpicturePalette(
+	VADriverContextP ctx,
+	VASubpictureID subpicture,
+	/*
+	 * pointer to an array holding the palette data.  The size of the array is
+	 * num_palette_entries * entry_bytes in size.  The order of the components
+	 * in the palette is described by the component_order in VASubpicture struct
+	 */
+	unsigned char *palette
+);
+
+VAStatus crystalhd_SetSubpictureChromakey(
+	VADriverContextP ctx,
+	VASubpictureID subpicture,
+	unsigned int chromakey_min,
+	unsigned int chromakey_max,
+	unsigned int chromakey_mask
+);
+
+VAStatus crystalhd_SetSubpictureGlobalAlpha(
+	VADriverContextP ctx,
+	VASubpictureID subpicture,
+	float global_alpha 
+);
+
+
+VAStatus crystalhd_AssociateSubpicture(
+	VADriverContextP ctx,
+	VASubpictureID subpicture,
+	VASurfaceID *target_surfaces,
+	int num_surfaces,
+	short src_x, /* upper left offset in subpicture */
+	short src_y,
+	unsigned short src_width,
+	unsigned short src_height,
+	short dest_x, /* upper left offset in surface */
+	short dest_y,
+	unsigned short dest_width,
+	unsigned short dest_height,
+	/*
+	 * whether to enable chroma-keying or global-alpha
+	 * see VA_SUBPICTURE_XXX values
+	 */
+	unsigned int flags
+);
+
+VAStatus crystalhd_DeassociateSubpicture(
+	VADriverContextP ctx,
+	VASubpictureID subpicture,
+	VASurfaceID *target_surfaces,
+	int num_surfaces
+);
+
+VAStatus crystalhd_CreateContext(
+	VADriverContextP ctx,
+	VAConfigID config_id,
+	int picture_width,
+	int picture_height,
+	int flag,
+	VASurfaceID *render_targets,
+	int num_render_targets,
+	VAContextID *context		/* out */
+);
+
+VAStatus crystalhd_DestroyContext(
+	VADriverContextP ctx,
+	VAContextID context
+);
+
+VAStatus crystalhd_CreateBuffer(
+	VADriverContextP ctx,
+	VAContextID context,		/* in */
+	VABufferType type,		/* in */
+	unsigned int size,		/* in */
+	unsigned int num_elements,	/* in */
+	void *data,			/* in */
+	VABufferID *buf_id		/* out */
+);
+
+VAStatus crystalhd_BufferSetNumElements(
+	VADriverContextP ctx,
+	VABufferID buf_id,		/* in */
+	unsigned int num_elements	/* in */
+);
+
+VAStatus crystalhd_MapBuffer(
+	VADriverContextP ctx,
+	VABufferID buffer_id,	/* in */
+	void **pbuf		/* out */
+);
+
+VAStatus crystalhd_UnmapBuffer(
+	VADriverContextP ctx,
+	VABufferID buf_id	/* in */
+);
+
+VAStatus crystalhd_DestroyBuffer(
+	VADriverContextP ctx,
+	VABufferID buffer_id
+);
+VAStatus crystalhd_BeginPicture(
+	VADriverContextP ctx,
+	VAContextID context,
+	VASurfaceID render_target
+);
+
+VAStatus crystalhd_RenderPicture(
+	VADriverContextP ctx,
+	VAContextID context,
+	VABufferID *buffers,
+	int num_buffers
+);
+
+VAStatus crystalhd_EndPicture(
+	VADriverContextP ctx,
+	VAContextID context
+);
+
+VAStatus crystalhd_SyncSurface(
+	VADriverContextP ctx,
+	VASurfaceID render_target
+);
+
+VAStatus crystalhd_QuerySurfaceStatus(
+	VADriverContextP ctx,
+	VASurfaceID render_target,
+	VASurfaceStatus *status	/* out */
+);
+
+VAStatus crystalhd_PutSurface(
+	VADriverContextP ctx,
+	VASurfaceID surface,
+	void *draw, /* Drawable of window system */
+	short srcx,
+	short srcy,
+	unsigned short srcw,
+	unsigned short srch,
+	short destx,
+	short desty,
+	unsigned short destw,
+	unsigned short desth,
+	VARectangle *cliprects, /* client supplied clip list */
+	unsigned int number_cliprects, /* number of clip rects in the clip list */
+	unsigned int flags /* de-interlacing flags */
+);
+
+/* 
+* Query display attributes 
+* The caller must provide a "attr_list" array that can hold at
+* least vaMaxNumDisplayAttributes() entries. The actual number of attributes
+* returned in "attr_list" is returned in "num_attributes".
+*/
+VAStatus crystalhd_QueryDisplayAttributes (
+	VADriverContextP ctx,
+	VADisplayAttribute *attr_list,	/* out */
+	int *num_attributes		/* out */
+);
+
+/* 
+ * Get display attributes 
+ * This function returns the current attribute values in "attr_list".
+ * Only attributes returned with VA_DISPLAY_ATTRIB_GETTABLE set in the "flags" field
+ * from vaQueryDisplayAttributes() can have their values retrieved.  
+ */
+VAStatus crystalhd_GetDisplayAttributes (
+	VADriverContextP ctx,
+	VADisplayAttribute *attr_list,	/* in/out */
+	int num_attributes
+);
+
+/* 
+ * Set display attributes 
+ * Only attributes returned with VA_DISPLAY_ATTRIB_SETTABLE set in the "flags" field
+ * from vaQueryDisplayAttributes() can be set.  If the attribute is not settable or 
+ * the value is out of range, the function returns VA_STATUS_ERROR_ATTR_NOT_SUPPORTED
+ */
+VAStatus crystalhd_SetDisplayAttributes (
+	VADriverContextP ctx,
+	VADisplayAttribute *attr_list,
+	int num_attributes
+);
+
+VAStatus crystalhd_BufferInfo(
+	VADriverContextP ctx,
+	VAContextID context,		/* in */
+	VABufferID buffer_id,		/* in */
+	VABufferType *type,		/* out */
+	unsigned int *size,		/* out */
+	unsigned int *num_elements	/* out */
+);
+
+#if 0
+VAStatus crystalhd_LockSurface(
+	VADriverContextP ctx,
+	VASurfaceID surface,
+	unsigned int *fourcc, /* following are output argument */
+	unsigned int *luma_stride,
+	unsigned int *chroma_u_stride,
+	unsigned int *chroma_v_stride,
+	unsigned int *luma_offset,
+	unsigned int *chroma_u_offset,
+	unsigned int *chroma_v_offset,
+	unsigned int *buffer_name,
+	void **buffer
+);
+
+VAStatus crystalhd_UnlockSurface(
+	VADriverContextP ctx,
+	VASurfaceID surface
+);
+#endif
+
+VAStatus
+crystalhd_Terminate(
+	VADriverContextP ctx
+);
+
+VAStatus
+__vaDriverInit_0_31(
+	VADriverContextP ctx
+);
 #endif
--- src/crystalhd_video_h264.h
+++ src/crystalhd_video_h264.h
@@ -31,64 +31,6 @@
 
 #include "crystalhd_drv_video.h"
 
-typedef struct {
-	int level_idc;
-	int mbps;		/* max macroblock processing rate (macroblocks/sec) */
-	int frame_size;		/* max frame size (macroblocks) */
-	int dpb;		/* max decoded picture buffer (bytes) */
-	int bitrate;		/* max bitrate (kbit/sec) */
-	int cpb;		/* max vbv buffer (kbit) */
-	int mv_range;		/* max vertical mv component range (pixels) */
-	int mvs_per_2mb;	/* max mvs per 2 consecutive mbs. */
-	int slice_rate;		/* ?? */
-	int mincr;		/* min compression ratio */
-	int bipred8x8;		/* limit bipred to >=8x8 */
-	int direct8x8;		/* limit b_direct to >=8x8 */
-	int frame_only;		/* forbid interlacing */
-} h264_level_t;
-
-enum h264_profile_e
-{
-	H264_PROFILE_BASELINE		= 66,
-	H264_PROFILE_MAIN		= 77,
-	H264_PROFILE_EXTENDED		= 88,
-	H264_PROFILE_HIGH		= 100,
-	H264_PROFILE_HIGH10		= 110,
-	H264_PROFILE_HIGH422		= 122,
-	H264_PROFILE_HIGH444		= 144,
-	H264_PROFILE_HIGH444_PREDICTIVE	= 244,
-};
-
-enum NALU
-{
-	NALU_PRIO_DISPOSABLE	= 0x00,
-	NALU_PRIO_NORMAL	= 0x20,
-	NALU_PRIO_HIGH		= 0x40,
-	NALU_PRIO_HIGHEST	= 0x60,
-
-	NALU_TYPE_UNSPECIFIED	= 0x00,
-	NALU_TYPE_PICTURE	= 0x01,
-	NALU_TYPE_SLICE_A	= 0x02,
-	NALU_TYPE_SLICE_B	= 0x03,
-	NALU_TYPE_SLICE_C	= 0x04,
-	NALU_TYPE_SLICE_IDR	= 0x05,
-	NALU_TYPE_SEI		= 0x06,
-	NALU_TYPE_SPS		= 0x07,
-	NALU_TYPE_PPS		= 0x08,
-	NALU_TYPE_AUD		= 0x09,
-	NALU_TYPE_EOSEQ		= 0x0a,
-	NALU_TYPE_EOS		= 0x0b,
-	NALU_TYPE_FILL		= 0x0c,
-	NALU_TYPE_SPSEXT	= 0x0d,
-	NALU_TYPE_PREFIX	= 0x0e,
-	NALU_TYPE_SUBSPS	= 0x0f,
-	NALU_TYPE_AUXPICTURE	= 0x13,
-	NALU_TYPE_CSEXT		= 0x14,
-
-	NALU_PRIORITY_MASK	= 0x60,
-	NALU_UNITTYPE_MASK	= 0x1f,
-};
-
 VAStatus
 crystalhd_render_iqmatrix_buffer_h264(
 	VADriverContextP ctx,
@@ -115,6 +57,13 @@ crystalhd_render_slice_data_buffer_h264(
 	VADriverContextP ctx,
 	object_context_p obj_context,
 	object_buffer_p obj_buffer
+);
+
+VAStatus
+crystalhd_begin_picture_h264(
+	VADriverContextP ctx,
+	VAContextID context,
+	VASurfaceID render_target
 );
 
 VAStatus
